98d01a26f1d2b805309fcff4abceb95fed459738
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2024-06-06 14:47:27 -0500

    Update ml_rotifer_tox.py

--- ml_rotifer_tox.py
+++ ml_rotifer_tox.py
@@ -945,7 +945,7 @@ if uploaded_file_1 is not None:
         data = pd.read_csv(uploaded_file_1,) 
         train_data = data_train[loaded_desc]
         # Calculate descriptors and SMILES for the first column
-        descriptors_total_1, smiles_list_1 = calc_descriptors(data, 3)
+        descriptors_total_1, smiles_list_1 = calc_descriptors(data, 1)
         # Calculate descriptors and SMILES for the second column
         #descriptors_total_2, smiles_list_2 = calc_descriptors(data, 4)
 
@@ -1001,7 +1001,7 @@ else:
         data = pd.read_csv("example_file1.csv")
         train_data = data_train[loaded_desc]
         # Calculate descriptors and SMILES for the first column
-        descriptors_total_1, smiles_list_1 = calc_descriptors(data, 3)
+        descriptors_total_1, smiles_list_1 = calc_descriptors(data, 1)
         # Calculate descriptors and SMILES for the second column
         #descriptors_total_2, smiles_list_2 = calc_descriptors(data, 4)
 
