89508e13e0156c6fc74a8ce35571a2b657a265e6
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2024-06-08 02:37:21 -0500

    Update ml_rotifer_tox.py

--- ml_rotifer_tox.py
+++ ml_rotifer_tox.py
@@ -1131,7 +1131,8 @@ if uploaded_file_1 is not None:
             st.subheader(r'pLC50 salt water')
             st.write(styled_df)
         with col2:
-            st.subheader("William's Plot (Applicability Domain)")
+            st.markdown("<h2 style='text-align: center;'>William's Plot (Applicability Domain)</h2>", unsafe_allow_html=True)
+            #st.subheader("William's Plot (Applicability Domain)")
             st.plotly_chart(figure,use_container_width=True)
         st.markdown(":point_down: **Here you can download the results**", unsafe_allow_html=True)
         st.markdown(filedownload1(final_file), unsafe_allow_html=True)
@@ -1186,7 +1187,8 @@ else:
             st.subheader(r'pLC50 salt water')
             st.write(styled_df)
         with col2:
-            st.subheader("William's Plot (Applicability Domain)")
+            st.markdown("<h2 style='text-align: center;'>William's Plot (Applicability Domain)</h2>", unsafe_allow_html=True)
+            #st.subheader("William's Plot (Applicability Domain)")
             st.plotly_chart(figure,use_container_width=True)
         st.markdown(":point_down: **Here you can download the results**", unsafe_allow_html=True,)
         st.markdown(filedownload1(final_file), unsafe_allow_html=True)
@@ -1250,7 +1252,8 @@ if on2:
             st.subheader(r'pLC50 salt water')
             st.write(styled_df)
         with col2:
-            st.subheader("William's plot (Applicability Domain)")
+            st.markdown("<h2 style='text-align: center;'>William's Plot (Applicability Domain)</h2>", unsafe_allow_html=True)
+            #st.subheader("William's plot (Applicability Domain)")
             st.plotly_chart(figure,use_container_width=True)
         st.markdown(":point_down: **Here you can download the results**", unsafe_allow_html=True,)
         st.markdown(filedownload1(final_file), unsafe_allow_html=True)
