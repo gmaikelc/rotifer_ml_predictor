a0ce11b2b9390ce4469c14a3d13b07f353e853f1
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2024-06-08 02:22:47 -0500

    Update ml_rotifer_tox.py

--- ml_rotifer_tox.py
+++ ml_rotifer_tox.py
@@ -996,13 +996,14 @@ def williams_plot(leverage_train, leverage_test, std_residual_train, std_residua
         #training_legend = Line2D([0],[0], color='cornflowerblue', marker='o', markersize=10, label='Training')
         #validation_legend =Line2D([0],[0],color='orange', marker='o', markersize=10, label='Validation')
         # add manual symbols to auto legend
-        handles.extend([training_legend, validation_legend])
+        #handles.extend([training_legend, validation_legend])
 
         # Custom legend                      
         training_legend = mpatches.Patch( color='cornflowerblue', marker='o', markersize=10, label='Training')
         validation_legend = mpatches.Patch(color='orange', marker='o', markersize=10, label='Validation')
         # handles is a list, so append manual patch
-        handles.append(patch) 
+        handles.append(training_legend)
+        handles.append(validation_legend)              
 
        
         plt.legend(handles=handles, bbox_to_anchor=(0.99, 0.34), loc='upper right',
