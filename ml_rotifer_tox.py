480fc065455f8eb80a44af18b4b28bd52ee6e950
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2024-06-08 02:33:18 -0500

    Update ml_rotifer_tox.py

--- ml_rotifer_tox.py
+++ ml_rotifer_tox.py
@@ -1186,7 +1186,7 @@ else:
             st.subheader(r'pLC50 salt water')
             st.write(styled_df)
         with col2:
-            st.header("William's Plot (Applicability Domain)")
+            st.subheader("William's Plot (Applicability Domain)")
             st.plotly_chart(figure,use_container_width=True)
         st.markdown(":point_down: **Here you can download the results**", unsafe_allow_html=True,)
         st.markdown(filedownload1(final_file), unsafe_allow_html=True)
@@ -1250,7 +1250,7 @@ if on2:
             st.subheader(r'pLC50 salt water')
             st.write(styled_df)
         with col2:
-            st.header("William's plot (Applicability Domain)")
+            st.subheader("William's plot (Applicability Domain)")
             st.plotly_chart(figure,use_container_width=True)
         st.markdown(":point_down: **Here you can download the results**", unsafe_allow_html=True,)
         st.markdown(filedownload1(final_file), unsafe_allow_html=True)
