9d4e7f7b8d49e9f581662193ab2c16a50dbb1fc2
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2024-06-09 20:26:51 -0500

    Update ml_rotifer_tox.py

--- ml_rotifer_tox.py
+++ ml_rotifer_tox.py
@@ -901,7 +901,7 @@ def williams_plot(leverage_train, leverage_test, std_residual_train, std_residua
                   ]
 
         # Create the figure
-        ax.legend(handles=legend_elements, loc='upper_right',bbox_to_anchor=(0.99, 0.34)
+        ax.legend(handles=legend_elements, loc='upper_right',bbox_to_anchor=(0.99, 0.34),
                  fontsize=14, edgecolor='black')          
 
              
