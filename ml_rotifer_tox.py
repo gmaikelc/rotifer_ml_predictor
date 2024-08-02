95494edb4b293a154de8e238ef0da9c0fc23fc27
Gerardo Casanola <39452660+gmaikelc@users.noreply.github.com>
2024-08-02 17:39:18 -0500

    Update ml_rotifer_tox.py

--- ml_rotifer_tox.py
+++ ml_rotifer_tox.py
@@ -1314,28 +1314,37 @@ else:
         data = pd.read_csv("example_file1.csv")
         
         train_data = data_train[loaded_desc]
-        #train_data2 = data_train2[loaded_desc2]
+        train_data2 = data_train2[loaded_desc2]
         
         # Calculate descriptors and SMILES for the first data
         descriptors_total_1, smiles_list_1 = calc_descriptors(data, 1)
         # Calculate descriptors and SMILES for the second data
-        #descriptors_total_2, smiles_list_2 = calc_descriptors(data, 1)
+        descriptors_total_2, smiles_list_2 = calc_descriptors(data, 1)
 
         
                  
         #Selecting the descriptors based on model for salt water component
         test_data1, id_list_1 =  reading_reorder(descriptors_total_1,loaded_desc)
         #Selecting the descriptors based on model for first component
-        #test_data2, id_list_2 =  reading_reorder(descriptors_total_2,loaded_desc2)
-                          
+        test_data2, id_list_2 =  reading_reorder2(descriptors_total_2,loaded_desc2)
+ 
+                         
         
         X_final2= test_data1
-        #X_final4 = test_data2
+        X_final4 = test_data2
+        
         df_train_normalized, df_test_normalized = normalize_data(train_data, X_final2)
+        df_train_normalized2, df_test_normalized2 = normalize_data2(train_data2, X_final4)
         #st.markdown(filedownload5(df_test_normalized), unsafe_allow_html=True)
+        
         final_file, styled_df,leverage_train,std_residual_train, leverage_test, std_residual_test= predictions(loaded_model, loaded_desc, df_test_normalized)
+        final_file2, styled_df2,leverage_train2,std_residual_train2, leverage_test2, std_residual_test2= predictions2(loaded_model2, loaded_desc2, df_test_normalized2)
+        
         x_lim_max_std, x_lim_min_std, h_critical, x_lim_max_lev, x_lim_min_lev = calculate_wp_plot_limits(leverage_train,std_residual_train, x_std_max=4, x_std_min=-4)
-        figure  = williams_plot(leverage_train, leverage_test, std_residual_train, std_residual_test)   
+        x_lim_max_std2, x_lim_min_std2, h_critical2, x_lim_max_lev2, x_lim_min_lev2 = calculate_wp_plot_limits2(leverage_train2,std_residual_train2, x_std_max2=4, x_std_min2=-4)
+        
+        figure  = williams_plot(leverage_train, leverage_test, std_residual_train, std_residual_test)
+        figure2  = williams_plot2(leverage_train2, leverage_test2, std_residual_train2, std_residual_test2)   
         col1, col2 = st.columns(2)
 
         with col1:
@@ -1349,15 +1358,14 @@ else:
         with col2:
             st.header("Fresh Water",divider='blue')
             st.subheader(r'Predictions')
-            st.write(styled_df)
+            st.write(styled_df2)
             st.markdown("<h2 style='text-align: center; font-size: 30px;'>William's Plot (Applicability Domain)</h2>", unsafe_allow_html=True)
-            st.plotly_chart(figure,use_container_width=True)
-            #st.subheader("William's plot (Applicability Domain)")
+            st.plotly_chart(figure2,use_container_width=True)
             st.markdown(":point_down: **Here you can download the results for Fresh Water model**", unsafe_allow_html=True,)
-            st.markdown(filedownload1(final_file), unsafe_allow_html=True)
-        
-       # st.markdown(":point_down: **Here you can download the results for Fresh Water Model**", unsafe_allow_html=True,)
-       # st.markdown(filedownload1(final_file), unsafe_allow_html=True)
+            st.markdown(filedownload1(final_file2), unsafe_allow_html=True)
+
+
+
         
 
 on2 = st.toggle('Use drawn structure',key="13")
@@ -1375,27 +1383,35 @@ if on2:
         data = pd.DataFrame({'ID': [ID], 'Smiles_1': [smiles_list]})
 
         train_data = data_train[loaded_desc]
-        # Calculate descriptors and SMILES for the first column
+        train_data2 = data_train2[loaded_desc2]
+        
+        # Calculate descriptors and SMILES for the first data
         descriptors_total_1, smiles_list_1 = calc_descriptors(data, 1)
-        # Calculate descriptors and SMILES for the second column
-        #descriptors_total_2, smiles_list_2 = calc_descriptors2(data, 1)
-
+        # Calculate descriptors and SMILES for the second data
+        descriptors_total_2, smiles_list_2 = calc_descriptors(data, 1)
         
                  
         #Selecting the descriptors based on model for salt water component
-        test_data1, id_list_1 =  reading_reorder(descriptors_total_1)
+        test_data1, id_list_1 =  reading_reorder(descriptors_total_1,loaded_desc)
         #Selecting the descriptors based on model for first component
-        #test_data2, id_list_1 =  reading_reorder2(descriptors_total_2)
- 
-                         
+        test_data2, id_list_2 =  reading_reorder2(descriptors_total_2,loaded_desc2)
+                          
         
         X_final2= test_data1
-        #X_final4 = test_data2
+        X_final4 = test_data2
+        
         df_train_normalized, df_test_normalized = normalize_data(train_data, X_final2)
+        df_train_normalized2, df_test_normalized2 = normalize_data2(train_data2, X_final4)
         #st.markdown(filedownload5(df_test_normalized), unsafe_allow_html=True)
+        
         final_file, styled_df,leverage_train,std_residual_train, leverage_test, std_residual_test= predictions(loaded_model, loaded_desc, df_test_normalized)
+        final_file2, styled_df2,leverage_train2,std_residual_train2, leverage_test2, std_residual_test2= predictions2(loaded_model2, loaded_desc2, df_test_normalized2)
+        
         x_lim_max_std, x_lim_min_std, h_critical, x_lim_max_lev, x_lim_min_lev = calculate_wp_plot_limits(leverage_train,std_residual_train, x_std_max=4, x_std_min=-4)
-        figure  = williams_plot(leverage_train, leverage_test, std_residual_train, std_residual_test)   
+        x_lim_max_std2, x_lim_min_std2, h_critical2, x_lim_max_lev2, x_lim_min_lev2 = calculate_wp_plot_limits2(leverage_train2,std_residual_train2, x_std_max2=4, x_std_min2=-4)
+        
+        figure  = williams_plot(leverage_train, leverage_test, std_residual_train, std_residual_test)
+        figure2  = williams_plot2(leverage_train2, leverage_test2, std_residual_train2, std_residual_test2)   
         col1, col2 = st.columns(2)
 
         with col1:
@@ -1409,12 +1425,13 @@ if on2:
         with col2:
             st.header("Fresh Water",divider='blue')
             st.subheader(r'Predictions')
-            st.write(styled_df)
+            st.write(styled_df2)
             st.markdown("<h2 style='text-align: center; font-size: 30px;'>William's Plot (Applicability Domain)</h2>", unsafe_allow_html=True)
-            st.plotly_chart(figure,use_container_width=True)
-            st.markdown(":point_down: **Here you can download the results for Fresh Water Model**", unsafe_allow_html=True,)
-            st.markdown(filedownload1(final_file), unsafe_allow_html=True)
-            
+            st.plotly_chart(figure2,use_container_width=True)
+            st.markdown(":point_down: **Here you can download the results for Fresh Water model**", unsafe_allow_html=True,)
+            st.markdown(filedownload1(final_file2), unsafe_allow_html=True)
+
+           
         
 
 #Footer edit
